005dc6be19f67d60ce570ba5c69bf6fb6a6246d3
admin <admin@LUDMILA>
2026-07-05 17:05:54 +0300

    ДЗ по yield return.Порівняння слів у двох масивах

---
+++ Book/YieldReturnHw/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace YieldReturnHw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = new string[] { "apple", "banana", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "nectarine", "orange", "mango" };
+            var array2 = new string[] { "apple", "cherry", "grape", "kiwi", "mango" };
+
+            var equalWords = array1.GetEqualWords(array2);
+
+            Console.WriteLine("Equal words:");
+            foreach (var word in equalWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
